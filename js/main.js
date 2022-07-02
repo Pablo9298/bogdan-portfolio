b540bfc952b0b5b2f4fd4bfb81357e87e54cd64a
Pavel B <wmpawka@gmail.com>
2022-07-02 20:50:49 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function () {
 
-  // $(".header__link, .header__scroll, .logo").on("click", function (event) {
-  //   event.preventDefault();
-  //   var id = $(this).attr('href'),
-  //     top = $(id).offset().top;
-  //   $('body,html').animate({
-  //     scrollTop: top
-  //   }, 1500);
-  // });
+  $(".header__link, .logo").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
 
   $('.header__burger').on('click', function () {
     $('.header__burger, .header__list, .header__social').toggleClass('active');
@@ -18,6 +18,22 @@ $(function () {
     $('.header__burger, .header__list, .header__social').removeClass('active');
     $('body').removeClass('lock');
   });
+
+  var headerTop = $('.header');
+  var doc = $(document);
+
+  function headerFixed() {
+    var threshold = doc.scrollTop() > 150;
+
+    if (threshold) {
+      headerTop.addClass('scrolled');
+    } else {
+      headerTop.removeClass('scrolled');
+    }
+  }
+  $(window).on('scroll', headerFixed);
+
+  headerFixed();
 
 
 });
